99096e52903df235502bc228b21023662d1e7949
Uros Markovic <uros.markovic@naissus.tech>
2023-11-28 10:25:02 +0100

    Adds ExamList into Exam Period

--- src/Core/Application/ExamPeriods/Queries/GetSingle/GetExamPeriodQueryHandler.cs
+++ src/Core/Application/ExamPeriods/Queries/GetSingle/GetExamPeriodQueryHandler.cs
@@ -4,6 +4,7 @@ using PastExamsHub.Base.Application.Common.Interfaces;
 using PastExamsHub.Core.Application.Common.Interfaces;
 using PastExamsHub.Core.Application.Courses;
 using PastExamsHub.Core.Application.ExamPeriods.Queries.GetCollection;
+using PastExamsHub.Core.Application.Exams.Models;
 using PastExamsHub.Core.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -31,7 +32,7 @@ namespace PastExamsHub.Core.Application.ExamPeriods.Queries.GetSingle
 
         public async Task<GetExamPeriodQueryResult> Handle(GetExamPeriodQuery request, CancellationToken cancellationToken)
         {
-            var result = await ExamPeriodRepository.
+            var period = await ExamPeriodRepository.
                  GetQuery()
                  .Where(x => x.Uid == request.Uid)
                  .Select(c => new ExamPeriodModel
@@ -44,7 +45,31 @@ namespace PastExamsHub.Core.Application.ExamPeriods.Queries.GetSingle
                  })
                  .SingleOrDefaultAsync(cancellationToken);
 
-            return new GetExamPeriodQueryResult { ExamPeriod = result };
+            var exams = await (
+                        from e in DbContext.Exams
+                        join ep in DbContext.ExamPeriods on e.Period.Id equals ep.Id
+                        join c in DbContext.Courses on e.Course.Id equals c.Id
+                        join l in DbContext.Users on c.Lecturer.Id equals l.Id
+                        where ep.Uid == period.Uid
+                        select new ExamModel
+                        {
+                            CourseName = c.Name,
+                            StudyYear = c.StudyYear,
+                            CourseType = c.CourseType,
+                            ESPB = c.ESPB,
+
+                            LecturerFirstName = l.FirstName,
+                            LecturerLastName = l.LastName,
+
+                            ExamDate = e.ExamDate,
+                            Notes = e.Notes,
+                            NumberOfTasks = e.NumberOfTasks,
+                            Type = e.Type,
+
+
+                        }).ToListAsync(cancellationToken);
+
+            return new GetExamPeriodQueryResult { ExamPeriod = period, Exams = exams };
         }
     }
 }
--- src/Core/Application/Exams/Command/Create/CreateExamCommandHandler.cs
+++ src/Core/Application/Exams/Command/Create/CreateExamCommandHandler.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation.Results;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using PastExamsHub.Base.Application.Common.Exceptions;
 using PastExamsHub.Base.Application.Common.Interfaces;
 using PastExamsHub.Core.Application.Common.Interfaces;
 using PastExamsHub.Core.Domain.Entities;
@@ -37,13 +39,53 @@ namespace PastExamsHub.Core.Application.Exams.Command.Create
         }
         public async Task<CreateExamCommandResult> Handle ( CreateExamCommand command, CancellationToken cancellationToken)
         {
-            var course = await CoursesRepository.GetByUidAsync(command.CourseUid, cancellationToken);
+
+            var validationFailures = new List<ValidationFailure>();
+
             var examPeriod = await ExamPeriodRepository
                 .GetQuery()
                 .Include(x=>x.Exams)
+                .ThenInclude(x=>x.Exam)
+                .ThenInclude(x=>x.Course)
                 .Where(x=>x.Uid==command.PeriodUid)
                 .SingleOrDefaultAsync();
 
+
+            var existingExxamCourseName = await(
+                from ep in DbContext.ExamPeriods
+                join e in DbContext.Exams on ep.Id equals e.Period.Id
+                join c in DbContext.Courses on e.Course.Id equals c.Id
+                where ep.Uid == command.PeriodUid && c.Uid == command.CourseUid
+                select c.Name
+                        ).SingleOrDefaultAsync(cancellationToken);
+
+
+            #region Validations
+
+            if (!string.IsNullOrEmpty(existingExxamCourseName))
+            {
+                validationFailures.Add(new ValidationFailure("Uid", "Exam with course: " + existingExxamCourseName + " already exist in "+examPeriod.Name +" period." ));
+            }
+
+            if (command.ExamDate <= examPeriod.StartDate)
+            {
+                validationFailures.Add(new ValidationFailure("ExamDate", "Exam date must be greather than period start date : " + examPeriod.StartDate+"."));
+            }
+
+            if (command.ExamDate >= examPeriod.EndDate)
+            {
+                validationFailures.Add(new ValidationFailure("ExamDate", "Exam date must be less than period end date : " + examPeriod.EndDate+".")); ;
+            }
+
+
+            if (validationFailures.Any())
+            {
+                var validationResult = new ValidationResult(validationFailures);
+                throw new ValidationException(validationResult.Errors);
+            }
+            #endregion Validations
+
+            var course = await CoursesRepository.GetByUidAsync(command.CourseUid, cancellationToken);
             //COMPLETE: add document
             var exam = new Exam(course, examPeriod, null, command.Type, command.ExamDate, command.NumberOfTasks, command.Notes);
             ExamRepository.Insert(exam);
